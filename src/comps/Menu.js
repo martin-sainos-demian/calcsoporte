cf82e499a66784a8882103255c1b03145685eff0
demmis98 <dem9847@gmail.com>
2023-02-24 11:50:02 -0600

    smthing

--- src/comps/Menu.js
+++ src/comps/Menu.js
@@ -1,6 +1,13 @@
+import { Routes, Route , HashRouter } from "react-router-dom";
+
 const menu = () => {
     return(
         <>
+            <HashRouter>
+                <Routes>
+                    <Route exact path='/' element={<p>a</p>>}></Route>
+                </Routes>
+            </HashRouter>
         </>
     )
 }
